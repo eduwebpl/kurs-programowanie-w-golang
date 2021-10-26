46a3dfe577fb17f19df2c3b248239b61c7fc6740
Piotr Krzesaj <piotr.krzesaj.5@gmail.com>
2021-10-26 08:24:11 +0200

    feat: add tworzenie serwera gin

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module 14szablony
+module 15gorm
 
 go 1.17
--- main.go
+++ main.go
@@ -1,49 +1,5 @@
 package main
 
-import (
-	"html/template"
-	"log"
-	"net/http"
-	"time"
-)
-
 func main() {
 
-	server := &http.Server{
-		Addr:         ":8080",
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  30 * time.Second,
-	}
-
-	assetDirectory := http.FileServer(http.Dir("./static/assets"))
-	imagesDirectory := http.FileServer(http.Dir("./static/images"))
-
-	http.Handle("/assets/", http.StripPrefix("/assets", assetDirectory))
-	http.Handle("/images/", http.StripPrefix("/images", imagesDirectory))
-	http.HandleFunc("/", rootPath)
-
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-type SiteTemplate struct {
-	Title string
-	Name  string
-}
-
-func rootPath(writer http.ResponseWriter, req *http.Request) {
-	siteData := SiteTemplate{
-		"Eduweb",
-		"Kamil",
-	}
-
-	tmplt, err := template.ParseFiles("./static/index.html")
-	if err != nil {
-		return
-	}
-
-	tmplt.Execute(writer, siteData)
 }
