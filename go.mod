3bf9b20f6aa6cb5a0b16fbdafc7ee27fda1b442b
Piotr Krzesaj <piotr.krzesaj.5@gmail.com>
2021-10-26 08:24:37 +0200

    feat: finish tworzenie serwera gin

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module 15gorm
 
 go 1.17
+
+require (
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,56 @@
 package main
 
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
 func main() {
 
+	db, err := gorm.Open("sqlite3", "database.db")
+
+	if err != nil {
+		panic("Failed to open the SQLite database.")
+	}
+
+	db.AutoMigrate(&User{})
+
+	// newUser := User{
+	// 	Name:        "Michał",
+	// 	LastName:    "",
+	// 	AccessToken: "",
+	// }
+	// db.Create(&newUser)
+
+	// result := db.Delete(&User{}, 1)
+	// if result.Error != nil {
+	// 	fmt.Println(result.Error)
+	// }
+
+	// users := []User{}
+	// result := db.Find(&users, "name = ?", "Michał")
+	// if result.Error != nil {
+	// 	if !errors.Is(result.Error, gorm.ErrRecordNotFound) {
+	// 		fmt.Println(result.Error)
+	// 		return
+	// 	}
+	// }
+	// fmt.Println(users)
+
+	// user := User{}
+	// user.ID = 2
+	// user.Name = "Michał2"
+	// user.LastName = "Nowak"
+
+	// result := db.Model(&user).Updates(user)
+	// if result.Error != nil {
+	// 	fmt.Println(result.Error)
+	// }
+}
+
+type User struct {
+	gorm.Model
+	Name        string `json:"name";gorm:"name"`
+	LastName    string `json:"lastName";gorm:"last_name"`
+	AccessToken string `json:"accessToken";gorm:"access_token"`
 }
