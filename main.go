9921db204947141eb91ece1f49059792777624f6
Piotr Krzesaj <piotr.krzesaj.5@gmail.com>
2021-10-26 08:11:54 +0200

    feat: finish struktury

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	rectangle := Rectangle{3, 3}
+	fmt.Println(rectangle)
+	rectangle.multiplyA(3)
+	fmt.Println(rectangle)
+}
+
+type Rectangle struct {
+	a int
+	b int
+}
+
+func (rectangle Rectangle) toSquareArea() int {
+	return rectangle.a * rectangle.b
+}
+
+func (rectangle *Rectangle) multiplyA(amount int) {
+	rectangle.a = rectangle.a * amount
 }
