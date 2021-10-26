2cafa94e0044f87f34e5fdf18eaf309b772ca184
Piotr Krzesaj <piotr.krzesaj.5@gmail.com>
2021-10-26 08:16:57 +0200

    feat: finish zmiana myslenia

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "time"
 
+func main() {
+	apiClient := Default("", time.Now())
+}
+
+type ApiClient interface {
+}
+
+type apiClient struct {
+	endpoint string
+	timeout  time.Time
+}
+
+func Default(endpoint string, timeout time.Time) ApiClient {
+	return apiClient{
+		endpoint,
+		timeout,
+	}
 }
