dd13b59e76109624a1494f805f23e7af7f3790f4
Piotr Krzesaj <piotr.krzesaj.5@gmail.com>
2021-10-26 08:12:46 +0200

    feat: finish interfejsy

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	cityTemperature := CityTemperature{[]float64{-1.8, -0.8, 2.9, 8.8, 14.2, 16.8, 19.2, 18.5, 13.8, 8.8, 3.2, -0.7}}
+	printCityInfo(cityTemperature)
+}
+
+func printCityInfo(city City) {
+	fmt.Println(city.averageTemperature())
+}
+
+type City interface {
+	averageTemperature() float64
+}
+
+type CityTemperature struct {
+	temperatures []float64
+}
+
+func (c CityTemperature) averageTemperature() float64 {
+	var sumOfTemps float64 = 0
+	for _, temperature := range c.temperatures {
+		sumOfTemps += temperature
+	}
+	return sumOfTemps / float64(len(c.temperatures))
 }
