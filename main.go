24b1589ed002902971a063fa3a158b0a942889c9
Piotr Krzesaj <piotr.krzesaj.5@gmail.com>
2021-10-26 08:20:26 +0200

    feat: finish tworzenie serwera

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
 func main() {
 
+	server := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  30 * time.Second,
+	}
+
+	http.HandleFunc("/", helloWorld)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func helloWorld(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Hello world"))
 }
